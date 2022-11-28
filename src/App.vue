a2eda62fd29b354da578e49906ce4e2149a20c56
Farhan Agigi <88557261+Farhanagigi@users.noreply.github.com>
2022-11-28 16:26:40 +0200

    feature: lifecycle timeout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <PrimeiroComponente />
   <p>O dado da app é: {{ testando }}</p>
+  <LifeCycle />
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import LifeCycle from "./components/LifeCycle.vue";
 import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+
 export default defineComponent({
   name: "App",
   components: {
     PrimeiroComponente,
+    LifeCycle,
   },
   data() {
     return {
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Meu nome é: {{ name }}</h1>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: "LifeCycle",
+  data() {
+    return {
+      name: "Ainda nao sei",
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.name = "Farhan";
+    }, 1000);
+  },
+  mounted() {
+    setTimeout(() => {
+      this.name = "Pedro";
+    }, 3000);
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
