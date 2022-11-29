0b9e5696ab6408f8b55698730bb2475abd92cda8
Farhan Agigi <88557261+Farhanagigi@users.noreply.github.com>
2022-11-29 14:22:58 +0200

    fix: conhecendo as directivas(v-if, v-show)

--- src/components/InfoR.vue
+++ src/components/InfoR.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <p>Estou trabalhando no momento.</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades</p>
     <p>Utilizo as seguintes tecnologias</p>
     <ul>
       <li>Javascript</li>
       <li>Java</li>
       <li>Vue</li>
     </ul>
+    <p v-show="mostrar_email">Mande uma mensagem para : {{ name }}</p>
   </div>
 </template>
 
@@ -15,6 +17,13 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "InfoR",
+  data() {
+    return {
+      esta_trabalhando: true,
+      mostrar_email: true,
+      name: "Farhan@email.com",
+    };
+  },
 });
 </script>
 
