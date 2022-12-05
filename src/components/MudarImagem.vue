3056a5a1c32dc627a2904d47f083e2c9f16fd721
Farhan Agigi <88557261+Farhanagigi@users.noreply.github.com>
2022-12-05 12:31:43 +0200

    emits

---
+++ src/components/MudarImagem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button @click="$emit('mudarImagem')">Trocar Imagem do perfil</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: "MudarImagem",
+  emits: ["mudarImagem"],
+});
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/PictureAvatar.vue
+++ src/components/PictureAvatar.vue
@@ -1,19 +1,30 @@
 <template>
   <div>
     <img :src="avatar" :alt="descricao" />
+    <MudarImagem @mudarImagem="trocaravatar" />
   </div>
 </template>
 
 <script>
 import { defineComponent } from "@vue/runtime-core";
+import MudarImagem from "./MudarImagem.vue";
 
 export default defineComponent({
   name: "PictureAvatar",
+  components: {
+    MudarImagem,
+  },
   data() {
     return {
-      avatar: "./img/icons8-customer-100.png",
+      avatar: "../img/icons8-customer-100.png/",
       descricao: "Farhan Agigi",
     };
+  },
+  methods: {
+    trocaravatar() {
+      this.avatar = "./img/male.png";
+      this.descricao = "Outro Usuario";
+    },
   },
 });
 </script>
