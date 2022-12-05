1e996028c9fce3f4dd9133ac8d0ba6faa9ff3184
Farhan Agigi <88557261+Farhanagigi@users.noreply.github.com>
2022-12-05 10:44:23 +0200

    renderizacao de lista (v-for)

--- src/components/InfoR.vue
+++ src/components/InfoR.vue
@@ -2,11 +2,18 @@
   <div>
     <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
     <p v-else>Estou em busca de novas oportunidades</p>
-    <p>Utilizo as seguintes tecnologias</p>
+    <p>Utilizo as seguintes tecnologias para back-end:</p>
     <ul>
-      <li>Javascript</li>
-      <li>Java</li>
-      <li>Vue</li>
+      <li v-for="technology in backend_technologies" :key="technology.id">
+        {{ technology }}
+      </li>
+    </ul>
+
+    <p>Utilizo as seguintes tecnologias para front-end:</p>
+    <ul>
+      <li v-for="technology in frontend_technologies" :key="technology.id">
+        {{ technology.language }}
+      </li>
     </ul>
 
     <div>
@@ -37,6 +44,12 @@ export default defineComponent({
       name: "Farhan@email.com",
       meu_link: "https://www.google.com",
       textoBotao: "Mostrar Email",
+      backend_technologies: ["Javascript", "Java", "Postgrey"],
+      frontend_technologies: [
+        { id: 1, language: "HTML" },
+        { id: 2, language: "CSS" },
+        { id: 3, language: "Vue" },
+      ],
     };
   },
   methods: {
